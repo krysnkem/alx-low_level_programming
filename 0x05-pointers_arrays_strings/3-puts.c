3cb61f1a7451bc76eeb0c1db3b496d8497b51773
krysnkem <krysnkem@gmail.com>
2022-03-22 01:36:43 +0000

    3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -8,10 +9,7 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
 
 void _puts(char *str)
 {
@@ -27,4 +25,8 @@ void _puts(char *str)
 		tmp = *(str + count);
 	}
 	_putchar('\n');
+}
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#include <unistd.h>
+
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _putchar(char c);
