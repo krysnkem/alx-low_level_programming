8b75bca559bfef3b918c057b6c92d28f28f01c9e
krysnkem <krysnkem@gmail.com>
2022-03-22 01:07:53 +0000

    Solved: 3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * _puts: prints a string, followed by a new line
+ * str: the string file
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+void _puts(char *str)
+{
+	char tmp;
+	int count;
+
+	tmp = *str;
+	count = 0;
+	while (tmp != '\0')
+	{
+		_putchar(tmp);
+		++count;
+		tmp = *(str + count);
+	}
+	_putchar('\n');
+}
