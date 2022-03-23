42fcc8a729183ebde478dea2d895fbc50517f23a
krysnkem <krysnkem@gmail.com>
2022-03-23 12:35:51 +0000

    3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- * _strlen - gets the length of a string.
  * print_rev - reverses the string.
  * @s: holds the array.
  * Return: length of string
@@ -9,28 +8,24 @@
 void print_rev(char *s)
 {
 	int alength;
-	int count;
+	int countIndex;
 	char tempVal;
-
-	alength = _strlen(s);
-	for (count = alength - 1; count > -1; count--)
-	{
-		tempVal = *(s + count);
-		_putchar(tempVal);
-	}
-	_putchar('\n');
-}
-int _strlen(char *s)
-{
 	char tmp;
 	int count;
-
+	
 	tmp = *s;
 	count = 0;
 	while (tmp != '\0')
 	{
 		++count;
 		tmp = *(s + count);
+        }
+
+	alength = count;
+	for (countIndex = alength - 1; countIndex > -1; countIndex--)
+	{
+		tempVal = *(s + countIndex);
+		_putchar(tempVal);
 	}
-	return (count);
+	_putchar('\n');
 }
