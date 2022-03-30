480a5603908fa46155f32f133c4725dd8f02d5ba
krysnkem <krysnkem@gmail.com>
2022-03-30 15:17:18 +0000

    Solution: 9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that prints a particular amount of array
+ * @dest: parameter that holds the destination location
+ * @src: holds the source string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char tmp;
+	int count;
+	int i;
+
+	tmp = *src;
+	count = 0;
+	while (tmp != '\0')
+	{
+		tmp = *(src + count + 1);
+		++count;
+	}
+	i = 0;
+	while (i < count)
+	{
+		*(dest + i) = *(src + i);
+		++i;
+	}
+	return dest;
+}
