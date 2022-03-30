e48caa125afcf8fec6b31829be1e830480a0782c
krysnkem <krysnkem@gmail.com>
2022-03-30 20:49:46 +0000

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
-/*
- * _puts_recursion- a function that prints a string recursively
+/**
+ * _puts_recursion - a function that prints a string recursively
  * @s: holds the address to the string
- * */
+ */
 void _puts_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
