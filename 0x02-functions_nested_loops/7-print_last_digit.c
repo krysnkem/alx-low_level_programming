6e7643576b2662057264f129a88b30b142251d23
Christian Onyisi <krysnkem@gmail.com>
2022-09-13 12:16:27 -0700

    Fix program that returns and prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,23 @@ int print_last_digit(int n)
 {
 	int lastDigit;
 
-	lastDigit = n % 10;
+	lastDigit = _abs(n % 10);
 	_putchar(lastDigit + '0');
 	return (lastDigit);
+}
+
+/**
+ * _abs - returns absolute value
+ * @n: operand parameter
+ *
+ * Description: returns absolute value of n
+ * Return: n absolute
+ *
+ *
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
 }
