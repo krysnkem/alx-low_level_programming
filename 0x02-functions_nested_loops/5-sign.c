42f208a2182a2b0d9ae1ba5f7fad57f07b169878
Christian Onyisi <krysnkem@gmail.com>
2022-09-13 11:39:26 -0700

    Add program that checks if a number is 0, greater than 0 or less than 0

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints +, 0 or -
+ * @n: parameter to check
+ *
+ * Description: Checks if n is zero, greater than zero
+ * or less than zero
+ * Return: 1 if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('+');
+		return (1);
+	}
+	_putchar('0');
+	return (0);
+}
