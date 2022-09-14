82e21a61e42a24c32e2cc54b11179b053733078a
Christian Onyisi <krysnkem@gmail.com>
2022-09-13 20:21:40 -0700

    Add a program that returns the addition of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers togetheri
+ * @n1: first number
+ * @n2: second number
+ *
+ * Description: calculates the addition of
+ * two number
+ * Return: n1 + n2
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ * main -Entry point
+ *
+ * Description: prints all alphabet
+ * in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
