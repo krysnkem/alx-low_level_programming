6415441213dc52955c0aad458c23894a2099e69a
Christian Onyisi <krysnkem@gmail.com>
2022-09-15 07:18:09 -0700

    Add a C file that contains a function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Description: finds the multiplication of two a and b
+ * Return: a * b
+ **/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
