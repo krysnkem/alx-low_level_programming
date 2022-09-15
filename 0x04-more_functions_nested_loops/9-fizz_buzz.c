45549475b2b26c493c01147dd4efe55e81874716
Christian Onyisi <krysnkem@gmail.com>
2022-09-15 11:40:35 -0700

    add a c program to print 1 to 100 with special appearance for multiples of 3 and 5

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  prints 1 - 100 but replaces
+ * mulitples of 5
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int num;
+
+	num = 1;
+
+	while (num <= 100)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf(" ");
+			printf("FizzBuzz");
+		}
+		else if (num % 3 == 0)
+		{
+			printf(" ");
+			printf("Fizz");
+		}
+		else if (num % 5 == 0)
+		{
+			printf(" ");
+			printf("Buzz");
+		}
+		else
+		{
+			if (num != 1)
+				printf(" ");
+			printf("%d", num);
+		}
+		++num;
+	}
+	return (0);
+}
