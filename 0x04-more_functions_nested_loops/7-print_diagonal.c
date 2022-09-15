7aa8dff1003bdd57d34e35605cd4af48471317d4
Christian Onyisi <krysnkem@gmail.com>
2022-09-15 08:58:38 -0700

    Add a C file the contains a function the prints a diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line
+ * @n: length of the diagonal
+ *
+ * Description: prints diagonal using '\'
+ **/
+void print_diagonal(int n)
+{
+	int space;
+	int line;
+	int s;
+
+	line = 0;
+	space = 0;
+	while (line < n)
+	{
+		s = 0;
+		while (s < space)
+		{
+			_putchar(' ');
+			++s;
+		}
+		_putchar('\\');
+		++line;
+		++space;
+		_putchar('\n')
+	}
+}
