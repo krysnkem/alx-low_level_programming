7c32120126c09c950e5400ae8c0b35b05779bcbc
Christian Onyisi <krysnkem@gmail.com>
2022-09-15 07:30:16 -0700

    Add a c file that contains a function that prints all natural numbers except 2 and 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints all numbers except 2 and 4
+ *
+ * Description: prints all natural numbers expept 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	num = '0';
+	while (num <= '9')
+	{
+		if (num == '2' || num == '4')
+		{
+			++num;
+			continue;
+		}
+		_putchar(num);
+		++num;
+	}
+	_putchar('\n');
+}
