69a0605430c6ff8caa3e1c3c8a7220bd7012cf00
Christian Onyisi <krysnkem@gmail.com>
2022-09-19 13:09:38 -0700

    Add a C ile containing a function that will reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: address of the first character of the string
+ **/
+void rev_string(char *s)
+{
+	int len;
+	int i;
+
+	len = _strlen(s);
+
+	int str[len];
+
+	i = len;
+
+	while (len >= 0)
+	{
+		--len;
+		str[i - (len + 1)] = *(s + len);
+	}
+
+	while (len < i)
+	{
+		*(s + len) = str[len];
+		++len;
+	}
+
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ *
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*(s + count) != '\0')
+	{
+		++count;
+	}
+
+	return (count);
+}
