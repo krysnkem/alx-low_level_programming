2889865f00fc49a3cc7ade603f1f6fbf6cfb5e3e
Christian Onyisi <krysnkem@gmail.com>
2022-09-19 13:42:28 -0700

    Add a c function that prints every other character starting from the first charcter

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * starting with the first character of a string
+ * @str: address of the first charcter of the string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		if (i % 2 != 0)
+		{
+			++i;
+			continue;
+		}
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
