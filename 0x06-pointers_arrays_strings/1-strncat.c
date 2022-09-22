c410b950265eb2105ab587303cdcbfdae50a7e0f
Christian Onyisi <krysnkem@gmail.com>
2022-09-22 09:32:23 -0700

    Add a C function that concatenate two strings based on a number a bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings together
+ * @dest: destination string
+ * @src: source string
+ * @n: bytes to copy
+ *
+ * Return: destination string
+ **/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int max, src_len, dest_len;
+	int i, pos;
+
+	src_len = _strlen(src);
+	dest_len = _strlen(dest);
+
+	if (n >= src_len)
+		max = src_len;
+	else
+		max = n;
+	i = 0;
+	while (i < max)
+	{
+		dest[dest_len + i] = src[i];
+		++i;
+	}
+	return (dest);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ *
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*(s + count) != '\0')
+	{
+		++count;
+	}
+	return (count);
+}
