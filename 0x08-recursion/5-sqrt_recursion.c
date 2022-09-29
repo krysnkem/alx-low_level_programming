19faa773e5097295a3426ae6f110272216d15ab8
Christian Onyisi <krysnkem@gmail.com>
2022-09-29 07:19:10 +0000

    Add a function that returns the natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+/**
+ * count_sub_odd_num - returns the number of times odd numbers
+ * could be subtracted
+ * @n: the number to subtract odd numbers from
+ * @x: how many times to subtract 2
+ *
+ * Return: returns the number of times odd numbers
+ */
+int count_sub_odd_num(int n, int x)
+{
+	int i;
+
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (-1 * (x + 1));
+	i = 0;
+	while (i < x)
+	{
+		n = n - 2;
+		++i;
+	}
+	return (1 + count_sub_odd_num(n - 1, x + 1));
+}
+/**
+ * _sqrt_recursion - gets the natural square root of a number
+ * @n: number to find the square root of
+ *
+ * Return: the square root of n or -1(if n doesn't have a natural square toot)
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (count_sub_odd_num(n, 0));
+}
