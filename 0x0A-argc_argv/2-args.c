bbd6b67ff69c2497bf951ff3e9ac0fd57c25aec7
Christian Onyisi <krysnkem@gmail.com>
2022-09-30 20:01:39 +0000

    Add a C program that prints the all arguments it receives including the first one

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: list of arguements
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int n = 0;
+	while (n < argc)
+		printf("%s\n", *(argv + n++));
+	return (0);
+}
