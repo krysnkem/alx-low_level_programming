aa5da50d72861a2d89908597d5cddb8c2dc4f79a
Christian Onyisi <krysnkem@gmail.com>
2022-10-06 16:07:57 +0000

    Fix a function that allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * _calloc - allocates memory for an array, using malloc
@@ -10,11 +11,22 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *array;
+	char *char_array;
+	unsigned int i;
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 	array = malloc(size * nmemb);
 	if (array == NULL)
 		return (NULL);
+	char_array = array;
+	if (char_array == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size * nmemb)
+	{
+		*(char_array + i) = 0;
+		++i;
+	}
 	return (array);
 }
