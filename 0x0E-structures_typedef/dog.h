d769c9720d3c0bb35e6eca1d089d59dbd433eaa2
Christian Onyisi <krysnkem@gmail.com>
2022-10-10 19:18:22 +0000

    Add a c header file that a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog struct type
+ * @name: name of dog
+ * @age: the age of the dog
+ * @owner: name of owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOG_H*/
