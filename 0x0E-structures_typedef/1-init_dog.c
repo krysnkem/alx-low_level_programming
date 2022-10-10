348d2f68d1e05081065fec8e71c54ffed7893b52
Christian Onyisi <krysnkem@gmail.com>
2022-10-10 19:36:32 +0000

    Fix c function that initialize a variable of type struct dog : update dog.h

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stdlib.h>
 /**
  * init_dog - initialize a variable of type struct dog
  * @d: pointer to type struct dog
@@ -9,6 +9,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
