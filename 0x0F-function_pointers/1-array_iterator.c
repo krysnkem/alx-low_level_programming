30a8e70386ccd226abaf7ebcf4d31e2393790fd2
Christian Onyisi <krysnkem@gmail.com>
2022-10-12 18:45:34 +0000

    Fix a function that executes a function given as a parameter on each element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i = 0;
 
-	if (action != NULL && size > 0)
+	if (action != NULL && size > 0
+			&& array != NULL)
 	{
 		for (i = 0; i < size; i++)
 			action(array[i]);
