d7237f5acdff3bde72894b627ad5fecea14735c2
Christian Onyisi <krysnkem@gmail.com>
2022-10-25 21:49:33 +0000

    Fix 9-insert_nodeint.c v2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = malloc(sizeof(listint_t));
 		(*head)->n = n;
 		(*head)->next = NULL;
+		return(*head);
 	}
 
 	while (h != NULL)
