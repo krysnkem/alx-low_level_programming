a6c77833da785aee1ef1e4b207df24090df8e2e0
Christian Onyisi <krysnkem@gmail.com>
2022-10-28 14:51:16 +0000

    Update 2-get_bit.c wit the correct code

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,6 +11,17 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int num = n;
 	unsigned int i = 0;
 
+	while (num != 0)
+	{
+		++i;
+		num >>= 1;
+	}
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	num = n;
+	i = 0;
+
 	while (i < index)
 	{
 		num >>= 1;
