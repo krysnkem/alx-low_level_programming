4137d6366427631d14c3242e2710966cd79eefb9
Christian Onyisi <krysnkem@gmail.com>
2022-10-29 20:22:42 +0000

    Add 4-clear_bit.c and update main.h

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+
+/**
+ * clear_bit - clears the value of a bit to 0 at a given index
+ * @index: the index to clear the bit
+ * @n: pointer to the number to clear the bit
+ *
+ * Return: 1 (success), -1 (failure)
+ */
+
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int bit_result = get_bit(*n, index);
+
+	if (bit_result == -1 || bit_result == 0)
+		return (-1);
+	*n = *n - _pow_recursion(2, index);
+	return (1);
+
+}
+
+/**
+ * get_bit - get the bit at a position in a number
+ * @n: the number to get the bit from
+ * @index: the index to get the bit from
+ *
+ * Return: the value of bit at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int num = n;
+	unsigned int i = 0;
+
+	if (index >= (sizeof(unsigned long int) * 4))
+		return (-1);
+
+	while (i < index)
+	{
+		num >>= 1;
+		++i;
+	}
+	num &= 1;
+
+	return (num);
+
+}
+
+/**
+ * _pow_recursion - calculates x raised to power y
+ * @x: base integer
+ * @y: power
+ *
+ * Return: 1 (if y == 0) or -1 (if y < 0) or x to power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}
