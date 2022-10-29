7ea87e3eebcb062cffe6c18add2e73fa0660b224
Christian Onyisi <krysnkem@gmail.com>
2022-10-29 18:44:05 +0000

    Add 3-set_bit.c and update main.h

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -16,7 +16,7 @@ int get_bit(unsigned long int n, unsigned int index)
 		++i;
 		num >>= 1;
 	}
-	if (index >= (sizeof(unsigned long int) * 8))
+	if (index >= (sizeof(unsigned long int) * 4))
 		return (-1);
 
 	num = n;
