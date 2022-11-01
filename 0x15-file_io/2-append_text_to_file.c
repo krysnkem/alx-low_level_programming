aa9998321826c610cb19c22af647a8debfd76430
Christian Onyisi <krysnkem@gmail.com>
2022-11-01 17:06:04 +0000

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		write_op = write(fd, text_content, strlen(text_content) - 2);
+		write_op = write(fd, text_content, strlen(text_content));
 		if (write_op < 0)
 			return (-1);
 	}
