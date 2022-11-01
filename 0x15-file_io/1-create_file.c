717eeb1c99af2bcb4a850623dd25601a4e8c2e36
Christian Onyisi <krysnkem@gmail.com>
2022-11-01 15:53:21 +0000

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,25 +13,21 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	ssize_t write_op;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
-	if (fd == -1 && errno == EEXIST)
-	{
-		fd = open(filename, O_TRUNC);
-		if (fd == -1)
-			return (-1);
-		return (-1);
-	}
-	if (fd == -1 && errno != EEXIST)
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
 		return (-1);
 
 	if (text_content == NULL)
-		return (-1);
-	write(fd, text_content, strlen(text_content));
+		text_content = "";
 
+	write_op = write(fd, text_content, strlen(text_content));
 	close(fd);
+	if (write_op < 0)
+		return (-1);
 	return (1);
 
 }
