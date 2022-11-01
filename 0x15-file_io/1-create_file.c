b2f481640ffe392a8da641778943e3aac1c6feb3
Christian Onyisi <krysnkem@gmail.com>
2022-11-01 15:36:45 +0000

    update 1-create_file.c containing a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		fd = open(filename, O_TRUNC);
 		if (fd == -1)
 			return (-1);
-		return (1);
+		return (-1);
 	}
 	if (fd == -1 && errno != EEXIST)
 		return (-1);
