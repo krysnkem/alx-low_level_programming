52f616842cb298f5452ace595008138a5f40aad9
Christian Onyisi <krysnkem@gmail.com>
2023-03-23 15:13:24 +0000

    Correct solution for Task 1 to comply with betty lint

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * dlistint_len - calculates the number of elements in a linked dlistint_t list.
+ * dlistint_len - calculates the number of elements in a linked
+ * dlistint_t list.
  * @h: pointer to the first node
  * Return: the number of nodes
  */
@@ -10,11 +11,11 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 
-	while(h)
+	while (h)
 	{
 		++count;
 		h = h->next;
 	}
 
-	return(count);
+	return (count);
 }
